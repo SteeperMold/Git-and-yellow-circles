9161dc15410315e7dff4599085d1a88ad1cfa692
Leamich <tennantonelove@yandex.ru>
2023-11-20 19:33:51 +0400

    Git и случайные окружности

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import sys
 import random
 
-from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication
 
+from UI import Ui_MainWindow
 
-class CircleDrawer(QWidget):
+
+class CircleDrawer(QWidget, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("UI.ui")
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.update)
 
     def paintEvent(self, event):
@@ -19,9 +20,9 @@ class CircleDrawer(QWidget):
         qp.end()
 
     def draw_circle(self, qp):
-        qp.setBrush(QColor(173, 255, 47))
+        qp.setBrush(QColor(*[random.randint(0, 255) for _ in range(3)]))
         radius = random.randint(50, 200)
-        qp.drawEllipse(200, 200, radius, radius)
+        qp.drawEllipse(50, 50, radius, radius)
 
 
 if __name__ == '__main__':
